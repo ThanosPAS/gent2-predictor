94f0510cef44f44fba70ec9dbcba7eb0cee51173
Paweł <pawel.darulewski@gmail.com>
2020-11-09 15:36:27 +0100

    Creating tensors and pickling them

--- gent2_predictor/data_parser/data_parser.py
+++ gent2_predictor/data_parser/data_parser.py
@@ -1,7 +1,10 @@
 import pickle
+import os
+import torch
+import errno
 
 import pandas as pd
-from gent2_predictor.settings import STRUCTURE_PATH, DATA_PATH
+from gent2_predictor.settings import STRUCTURE_PATH, DATA_PATH, DATA_DIR
 
 
 class DataParser:
@@ -28,4 +31,23 @@ class DataParser:
 
     @staticmethod
     def pickle_data(data):
-        pickle.dump(data, open('data.p', 'wb'))
+        for patient in data['patient'].unique():
+            patient_data = data.loc[data['patient'] == patient]
+            cancer_type = patient_data['cancer_type'].unique()[0]
+            patient_data = patient_data['expression'].values
+            tensor = torch.from_numpy(patient_data)
+            # target = torch.zeros(149 * 149)
+            # target[:21920] = tensor
+            # target = torch.reshape(target, (149, 149))
+            # target = normalize(target)
+
+            path = os.path.join(DATA_DIR, cancer_type)
+
+            if not os.path.exists(path):
+                try:
+                    os.makedirs(path)
+                except OSError as exc:
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+            pickle.dump(tensor, open(os.path.join(path, f'{patient}.p'), 'wb'))
